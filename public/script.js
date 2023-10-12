2d9905f996858e142965d85a0b253b9dfeed497d
Dcode24 <d.ogunseye7@gmail.com>
2023-10-12 15:04:38 +0100

    add project files to git

---
+++ public/script.js
@@ -0,0 +1,30 @@
+// Alert Box Display Sucess/Failure
+window.onload = function () {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  if (urlParams.has("success")) {
+    alert("Reservation successful");
+  } else if (urlParams.has("error")) {
+    alert(
+      "There was an error in making your reservation. Please call 07933402445"
+    );
+  }
+};
+
+// Tabbed Menu
+function openMenu(event, menuName) {
+  let menuArray = document.getElementsByClassName("menu");
+  for (let i = 0; i < menuArray.length; i++) {
+    menuArray[i].style.display = "none";
+  }
+
+  let tablinks = document.getElementsByClassName("tablink");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active-tab");
+  }
+
+  document.getElementById(menuName).style.display = "block";
+  event.currentTarget.classList.add("active-tab");
+}
+
+document.getElementById("mainLink").click();
